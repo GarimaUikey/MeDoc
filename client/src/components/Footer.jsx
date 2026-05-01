714d651dbed03ccab5d86a43a041c88add40a643
AKASH AHIRWAR <akashahirwar9976@gmail.com>
2026-05-01 15:25:46 +0530

    folder structure modification

--- client/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <div className="mt-12 bg-white p-6 text-center text-gray-500">
-      © 2024 MeDoc. All rights reserved.
-    </div>
-  );
-}
-
-export default Footer;
--- client/src/features/auth/authAPI.js
+++
@@ -1,9 +0,0 @@
-import API from "../../services/axiosInstance";
-
-export const loginUser = (data) => {
-  return API.post("/auth/login", data);
-};
-
-export const registerUser = (data) => {
-  return API.post("/auth/register", data);
-};
