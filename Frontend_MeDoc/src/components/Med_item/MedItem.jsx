f90588a280c9da37c2126cd67d53901fc46732f2
AKASH AHIRWAR <akashahirwar9976@gmail.com>
2026-05-20 15:57:38 +0530

    Add to cart Button added

--- Frontend_MeDoc/src/components/Med_item/MedItem.jsx
+++ Frontend_MeDoc/src/components/Med_item/MedItem.jsx
@@ -1,44 +1,149 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import './MedItem.css'
 import { AppContext } from '../../context/AppContext'
 import { assets } from '../../assets/assets'
 import { StoreContext } from '../../context/StoreContext'
 
-const MedItem = ({id,med_name,price,description,image}) => {
+const MedItem = ({ id, med_name, price, description, image }) => {
 
-    const {currencySymbol} = useContext(AppContext)
-  
-    const {cartItems,addToCart,removeFromCart}= useContext(StoreContext);
+    const { currencySymbol } = useContext(AppContext)
 
+    const {
 
+        cartItems,
 
-  return (
-    <div className='med-item'>
-        <div className="med-item-img-container">
-            <img className='med-item-image' src={image} alt="" />
-            {!cartItems[id]
-                ? <img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white}  alt=""/>
-                : <div className="med-item-counter">
-                    <img onClick={()=>removeFromCart(id)}  src={assets.remove_icon_red} alt="" />
-                    <p>{cartItems[id]}</p>
-                    <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
+        addToCart,
+
+        removeFromCart
+
+    } = useContext(StoreContext);
+
+    return (
+
+        <div className='med-item'>
+
+            {/* Medicine Image */}
+
+            <div className="med-item-img-container">
+
+                <img
+
+                    className='med-item-image'
+
+                    src={image}
+
+                    alt=""
+
+                />
+
+            </div>
+
+            {/* Medicine Info */}
+
+            <div className='med-item-info'>
+
+                <div className='med-item-name-rating'>
+
+                    <p>{med_name}</p>
+
+                    <img
+
+                        src={assets.rating_stars}
+
+                        alt=""
+
+                    />
+
                 </div>
 
+                <p className="med-item-description">
 
-            }
-        </div>
-        <div className='med-item-info'>
-            <div className='med-item-name-rating'>
-                <p>{med_name}</p>
-                <img src={assets.rating_stars} alt="" />
+                    {description}
+
+                </p>
+
+                {/* Price + Cart Section */}
+
+                <div className='flex items-center justify-between mt-4 gap-3'>
+
+                    {/* Price */}
+
+                    <p className="med-item-price">
+
+                        {currencySymbol} {price}
+
+                    </p>
+
+                    {/* Cart Controls */}
+
+                    {
+
+                        !cartItems[id]
+
+                            ? (
+
+                                <button
+
+                                    onClick={() => addToCart(id)}
+
+                                    className='bg-primary text-white px-4 py-2 rounded-full text-sm hover:opacity-90 transition-all duration-300'
+
+                                >
+
+                                    Add to Cart
+
+                                </button>
+
+                            )
+
+                            : (
+
+                                <div className="flex items-center gap-3 bg-gray-100 px-3 py-2 rounded-full">
+
+                                    <button
+
+                                        onClick={() => removeFromCart(id)}
+
+                                        className='w-7 h-7 rounded-full bg-red-100 text-red-500 text-lg font-bold hover:scale-110 transition-all duration-300'
+
+                                    >
+
+                                        -
+
+                                    </button>
+
+                                    <p className='font-medium min-w-[20px] text-center'>
+
+                                        {cartItems[id]}
+
+                                    </p>
+
+                                    <button
+
+                                        onClick={() => addToCart(id)}
+
+                                        className='w-7 h-7 rounded-full bg-green-100 text-green-500 text-lg font-bold hover:scale-110 transition-all duration-300'
+
+                                    >
+
+                                        +
+
+                                    </button>
+
+                                </div>
+
+                            )
+
+                    }
+
+                </div>
+
             </div>
-            <p className="med-item-description">{description} </p>
-            <p className="med-item-price">{currencySymbol} {price} </p>
 
         </div>
-      
-    </div>
-  )
+
+    )
+
 }
 
 export default MedItem
